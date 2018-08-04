c81743c8ef4d1a68cf1cf8f254648014f9db0326
muralimandula <murali.mandula@msitprogram.net>
2018-08-04 11:19:07 -0700

    " -> 3 of 7 passed. pylint: 10.00/10 "

---
+++ m6/p3/digit_product.py
@@ -0,0 +1,13 @@
+'''
+Given a  number int_input, find the product of all the digits
+example:
+	input: 123
+	output: 6
+'''
+PRODUCT = 1
+NUM = int(input())
+while NUM >= 1:
+    REM = int(NUM)%10
+    PRODUCT = PRODUCT * REM
+    NUM = NUM/10
+print(PRODUCT)
