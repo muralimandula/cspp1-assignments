82b329ea7760711ba2dca3cc2a995f888a0acbd7
muralimandula <murali.mandula@msitprogram.net>
2018-08-03 23:53:04 -0700

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- m4/p2/bob_counter.py
+++ m4/p2/bob_counter.py
@@ -1,9 +1,4 @@
-"""Assume s is a string of lower case characters.
-Write a program that prints the number of times the string 'bob' occurs in s.
-For example, if s = 'azcbobobegghakl', then your program should print
-Number of times bob occurs is: 2'''
-# the input string is in s
-# remove pass and start your code here"""
+"""Assume s is a string of lower case characters."""
 STR_A = input()
 SUB_A = 'bob'
 C_OUNT = 0
