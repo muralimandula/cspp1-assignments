367ddc88ae5728dd85ee58b3b1aba73df42935b1
muralimandula <murali.mandula@msitprogram.net>
2018-08-10 23:50:42 -0700

    " -> 3 of 3 passed. pylint: 9.05/10 "

--- m11/p3/assignment3.py
+++ m11/p3/assignment3.py
@@ -2,9 +2,13 @@
 '''
 At this point, we have written code to generate a random hand and display that hand to the user. We can also ask the user for a word (Python's input) and score the word (using your getWordScore). However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game. A valid word is in the word list; and it is composed entirely of letters from the current hand. Implement the isValidWord function.
 
-Testing: Make sure the test_isValidWord tests pass. In addition, you will want to test your implementation by calling it multiple times on the same hand - what should the correct behavior be? Additionally, the empty string ('') is not a valid word - if you code this function correctly, you shouldn't need an additional check for this condition.
+Testing: Make sure the test_isValidWord tests pass. In addition,
+you will want to test your implementation by calling it multiple times on the same hand
+what should the correct behavior be? Additionally, the empty string ('') is not a valid word
+if you code this function correctly, you shouldn't need an additional check for this condition.
 
-Fill in the code for isValidWord in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py before pasting your function definition here.
+Fill in the code for isValidWord in ps4a.py and be sure you've passed the
+appropriate tests in test_ps4a.py before pasting your function definition here.
 '''
 
 def is_word_valid(input_word, list_hand, word_list):
@@ -30,16 +34,17 @@ def is_word_valid(input_word, list_hand, word_list):
 
 
 def main():
+    """Main Function"""
     input_word = input()
     length_of_hand = int(input())
-    adict={}
+    adict = {}
     for _ in range(length_of_hand):
-        data=input()
+        data = input()
         list_hand = data.split()
         adict[list_hand[0]] = int(list_hand[1])
     word_list = input().split()
     print(is_word_valid(input_word, adict, word_list))
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
