b589b4ea19b7a441aeb6fc5ef767feddb1221a95
muralimandula <murali.mandula@msitprogram.net>
2018-08-11 11:29:00 -0700

    " -> 0 of 3 passed. pylint: 0/0 "

--- m12/p1/create_social_network.py
+++ m12/p1/create_social_network.py
@@ -4,7 +4,7 @@
 '''
 """
 
-def create_social_network(data):
+#def create_social_network(data):
     '''
         The data argument passed to the function is a string
         It represents simple social network data
@@ -44,16 +44,14 @@ def main():
 
     input_lines = input()
     adict = {}
-    for i in range(int(input_lines)):
+    for _ in range(int(input_lines)):
         data = input()
         if 'follows' in data:
             list_value = data.split(" follows ")
             if list_value[0] not in adict:
                 adict[list_value[0]] = list_value[1].split(',')
             else:
-    #          adict[list_value[0]] = list_value[1].append(list_value[1].split(','))
                adict[list_value[0]].extend(list_value[1].split(','))
-        # print(create_social_network(adict))
         else:
             break
     print(adict)
