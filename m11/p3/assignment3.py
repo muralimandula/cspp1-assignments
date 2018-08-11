c577a2681ccc63e54732ebd049d981b8a771fe62
muralimandula <murali.mandula@msitprogram.net>
2018-08-10 23:51:29 -0700

    " -> 3 of 3 passed. pylint: 9.52/10 "

--- m11/p3/assignment3.py
+++ m11/p3/assignment3.py
@@ -1,6 +1,7 @@
 # Assignment-3
 '''
-At this point, we have written code to generate a random hand and display that hand to the user. We can also ask the user for a word (Python's input) and score the word (using your getWordScore). However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game. A valid word is in the word list; and it is composed entirely of letters from the current hand. Implement the isValidWord function.
+At this point, we have written code to generate a random hand
+and display that hand to the user. We can also ask the user for a word (Python's input) and score the word (using your getWordScore). However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game. A valid word is in the word list; and it is composed entirely of letters from the current hand. Implement the isValidWord function.
 
 Testing: Make sure the test_isValidWord tests pass. In addition,
 you will want to test your implementation by calling it multiple times on the same hand
@@ -15,9 +16,7 @@ def is_word_valid(input_word, list_hand, word_list):
     """
     Returns True if word is in the wordList and is entirely
     composed of letters in the hand. Otherwise, returns False.
-
     Does not mutate hand or wordList.
-   
     word: string
     hand: dictionary (string -> int)
     wordList: list of lowercase strings
