cee70de7195ee00923e7d91478b3859d4c98add7
muralimandula <murali.mandula@msitprogram.net>
2018-08-10 23:28:09 -0700

    " -> 3 of 3 passed. pylint: 0.48/10 "

--- m11/p3/assignment3.py
+++ m11/p3/assignment3.py
@@ -7,7 +7,7 @@ Testing: Make sure the test_isValidWord tests pass. In addition, you will want t
 Fill in the code for isValidWord in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py before pasting your function definition here.
 '''
 
-def isValidWord(word, hand, wordList):
+def isValidWord(word, hand, word_list):
     """
     Returns True if word is in the wordList and is entirely
     composed of letters in the hand. Otherwise, returns False.
@@ -19,21 +19,28 @@ def isValidWord(word, hand, wordList):
     wordList: list of lowercase strings
     """
     # TO DO ... <-- Remove this comment when you code this function
-    
+    count = 0
+    for char in word:
+        if char in hand:
+            count += 1
+    if count == len(word):
+        if word in word_list:
+            return True
+    return False
 
 
 def main():
-	word=input()
-	n=int(input())
-	adict={}
-	for i in range(n):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	l2=input().split()
-	print(isValidWord(word,adict,l2))
-		
+    word=input()
+    n=int(input())
+    adict={}
+    for i in range(n):
+        data=input()
+        l=data.split()
+        adict[l[0]]= int(l[1])
+    l2=input().split()
+    print(isValidWord(word, adict, l2))
+        
 
 
 if __name__== "__main__":
-	main()
+    main()
