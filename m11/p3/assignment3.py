8d955a29c26c5a0690dc5adb22f72bb8a535829e
muralimandula <murali.mandula@msitprogram.net>
2018-08-10 23:52:39 -0700

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- m11/p3/assignment3.py
+++ m11/p3/assignment3.py
@@ -2,7 +2,8 @@
 '''
 At this point, we have written code to generate a random hand and display that hand to the user.
 We can also ask the user for a word (Python's input) and score the word (using your getWordScore).
-However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game.
+However, at this point we have not written any code to verify
+that a word given by a player obeys the rules of the game.
 A valid word is in the word list; and it is composed entirely of letters from the current hand.
 Implement the isValidWord function.
 
