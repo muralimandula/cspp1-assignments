d116bc8b8c0c72eecfaa3b289bcc4d4e2b8663b9
muralimandula <murali.mandula@msitprogram.net>
2018-08-10 22:29:30 -0700

    " -> 9 of 9 passed. pylint: 6.43/10 "

--- m11/p1/assignment1.py
+++ m11/p1/assignment1.py
@@ -20,8 +20,8 @@ def get_word_score(input_word, word_length):
     """
     # TO DO ... <-- Remove this comment when you code this function
     letter_values = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 
-    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 
-    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+        'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 
+        's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
 
     word_score = 0
     for char in input_word:
