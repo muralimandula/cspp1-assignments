c5f5a417f4199129389f7230b23fbd24d6fbe864
muralimandula <murali.mandula@msitprogram.net>
2018-08-11 11:33:06 -0700

    " -> 3 of 3 passed. pylint: 8.57/10 "

--- m12/p1/create_social_network.py
+++ m12/p1/create_social_network.py
@@ -1,61 +1,61 @@
 """
 '''
-	Assignment-1 Create Social Network
+    Assignment-1 Create Social Network
 '''
 
 
 #def create_social_network(data):
-	'''
-		The data argument passed to the function is a string
-		It represents simple social network data
-		In this social network data there are people following other people
+    '''
+        The data argument passed to the function is a string
+        It represents simple social network data
+        In this social network data there are people following other people
 
-		Here is an example social network data string:
-		John follows Bryant,Debra,Walter
-		Bryant follows Olive,Ollie,Freda,Mercedes
-		Mercedes follows Walter,Robin,Bryant
-		Olive follows John,Ollie
+        Here is an example social network data string:
+        John follows Bryant,Debra,Walter
+        Bryant follows Olive,Ollie,Freda,Mercedes
+        Mercedes follows Walter,Robin,Bryant
+        Olive follows John,Ollie
 
-		The string has multiple lines and each line represents one person
-		The first word of each line is the name of the person
-		The second word is follows that separates the person from the followers
-		After the second word is a list of people separated by ,
+        The string has multiple lines and each line represents one person
+        The first word of each line is the name of the person
+        The second word is follows that separates the person from the followers
+        After the second word is a list of people separated by ,
 
-		create_social_network function should split the string on lines
-		then extract the person and the followers by splitting each line
-		finally add the person and the followers to a dictionary and
-		return the dictionary
+        create_social_network function should split the string on lines
+        then extract the person and the followers by splitting each line
+        finally add the person and the followers to a dictionary and
+        return the dictionary
 
-		Caution: watch out for trailing spaces while splitting the string.
-		It may cause your test cases to fail although your output may be right
+        Caution: watch out for trailing spaces while splitting the string.
+        It may cause your test cases to fail although your output may be right
 
-		Error handling case:
-		Return a empty dictionary if the string format of the data is invalid
-		Empty dictionary is not None, it is a dictionary with no keys
-	'''
+        Error handling case:
+        Return a empty dictionary if the string format of the data is invalid
+        Empty dictionary is not None, it is a dictionary with no keys
+    '''
 
-	# remove the pass below and start writing your code
+    # remove the pass below and start writing your code
 #    return data"""
 
 def main():
-	'''
-		handling testcase input and printing output
-	'''
-	input_lines = input()
-	adict = {}
-	for i in range(int(input_lines)):
-		data = input()
-		if 'follows' in data:
-			list_value = data.split(" follows ")
-			if list_value[0] not in adict:
-				adict[list_value[0]] = list_value[1].split(',')
-			else:
-	#          adict[list_value[0]] = list_value[1].append(list_value[1].split(','))
-			   adict[list_value[0]].extend(list_value[1].split(','))
-		# print(create_social_network(adict))
-		else:
-			break
-	print(adict)
+    '''
+        handling testcase input and printing output
+    '''
+    input_lines = input()
+    adict = {}
+    for i in range(int(input_lines)):
+        data = input()
+        if 'follows' in data:
+            list_value = data.split(" follows ")
+            if list_value[0] not in adict:
+                adict[list_value[0]] = list_value[1].split(',')
+            else:
+    #          adict[list_value[0]] = list_value[1].append(list_value[1].split(','))
+               adict[list_value[0]].extend(list_value[1].split(','))
+        # print(create_social_network(adict))
+        else:
+            break
+    print(adict)
 
 if __name__ == "__main__":
-	main()
+    main()
