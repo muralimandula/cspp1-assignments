ad53cddb220eb9499c25f67d5d663ba0c2f25acd
muralimandula <murali.mandula@msitprogram.net>
2018-08-16 09:31:00 -0700

    " -> 1 of 8 passed. pylint: 8.12/10 "

--- m15.0/poker v2/poker_v2_praveen.py
+++ m15.0/poker v2/poker_v2_praveen.py
@@ -3,24 +3,6 @@
     Read about poker hands here.
     https://en.wikipedia.org/wiki/List_of_poker_hands
 '''
-# deck = {'diamond_red': ['2D','3D','4D','5D','6D','7D','8D','9D','TD','JD','QD','KD','AD'],
-# 'heart_red' : ['2H','3H','4H','5H','6H','7H','8H','9H','TH','JH','QH','KH','AH'],
-# 'club_black': ['2C','3C','4C','5C','6C','7C','8C','9C','TC','JC','QC','KC','AC'],
-# 'spade_black' : ['2S','3S','4S','5S','6S','7S','8S','9S','TS','JS','QS','KS','AS']}
-
-def int_cast(card_value):
-    if card_value[0] == 'T':
-        return 10
-    elif card_value[0] == 'J':
-        return 11
-    elif card_value[0] == 'Q':
-        return 12
-    elif card_value[0] == 'K':
-        return 13
-    elif card_value[0] == 'A':
-        return 14
-    return int(card_value)
-
 
 def is_straight(hand):
     '''
@@ -32,18 +14,7 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
-    hand = sorted(hand, key=int_cast)
-    print(hand)
-    straight_count = 0
-    for i in range(len(hand)-1):
-        if int_cast(hand[i][0])+1 == int_cast(hand[i+1][0]):
-            straight_count += 1
-    print(straight_count)
-    if straight_count == len(hand)-1:
-        return True
-    False
-
-
+    pass
 
 def is_flush(hand):
     '''
@@ -54,19 +25,7 @@ def is_flush(hand):
         Think of an algorithm: given the card suite how to check if it is a flush
         Write the code for it and return True if it is a flush else return False
     '''
-    for i in range(len(hand)-1):
-        if hand[i][1] != hand[i+1][1]:
-            return False
-    return True
-    # for suite in deck:
-    #     flush_count = 0
-    #     for card_value in hand:
-    #         if card_value in deck[suite]:
-    #             flush_count +=1
-    # print(flush_count)
-    # if flush_count == len(hand)-1:
-    #     return True
-    # return False
+    pass
 
 def hand_rank(hand):
     '''
@@ -85,17 +44,6 @@ def hand_rank(hand):
     # What would be the logic to determine if a hand is a straight or flush?
     # Let's not think about the logic in the hand_rank function
     # Instead break it down into two sub functions is_straight and is_flush
-    flush = is_flush(hand)
-    straight = is_straight(hand)
-    if flush and straight == True:
-        return 3
-
-    elif flush == True:
-        return 2
-
-    elif straight == True:
-        return 1
-    return 0
 
     # check for straight, flush and straight flush
     # best hand of these 3 would be a straight flush with the return value 3
@@ -103,6 +51,7 @@ def hand_rank(hand):
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
     # max in poker function uses these return values to select the best hand
+    return 1
 
 def poker(hands):
     '''
@@ -114,8 +63,7 @@ def poker(hands):
 
         Output: Return the winning poker hand
     '''
-    # print(hands)
-    
+
     # the line below may be new to you
     # max function is provided by python library
     # learn how it works, in particular the key argument, from the link
@@ -123,7 +71,7 @@ def poker(hands):
     # hand_rank is a function passed to max
     # hand_rank takes a hand and returns its rank
     # max uses the rank returned by hand_rank and returns the best hand
-    return max(hands, key=hand_rank)  #will return only one max list
+    return max(hands, key=hand_rank)
 
 if __name__ == "__main__":
     # read the number of test cases
