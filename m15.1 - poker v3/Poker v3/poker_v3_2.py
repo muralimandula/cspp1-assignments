4bd5950d332016548c0642df88d861b921bb65e6
muralimandula <murali.mandula@msitprogram.net>
2018-08-16 22:57:16 -0700

    " -> 0 of 19 passed. pylint: 8.12/10 "

--- m15.1 - poker v3/Poker v3/poker_v3_2.py
+++ m15.1 - poker v3/Poker v3/poker_v3_2.py
@@ -5,19 +5,19 @@
 '''
 # import collections
 
-def crd_v(hand):
+def card_value_hand(hand):
     '''calculates card values'''
-    excep = [2, 3, 4, 5, 14]
-    crd_val = ['--23456789TJQKA'.index(c) for c, s in hand]
-    if crd_val == excep:
-        crd_val.remove(14)
-        crd_val.add(1)
-    crd_val = sorted(crd_val, reverse=True)
-    return crd_val
+    exception_case = [2, 3, 4, 5, 14]
+    card_value = ['--23456789TJQKA'.index(c) for c, s in hand]
+    if card_value == exception_case:
+        card_value.remove(14)
+        card_value.add(1)
+    card_value = sorted(card_value, reverse=True)
+    return card_value
 
 def is_straight(hand):
     ''' Straight hand function '''
-    return len(crd_v(hand)) == 5 and (max(crd_v(hand))-min(crd_v(hand)) == 4)
+    return len(card_value_hand(hand)) == 5 and (max(card_value_hand(hand))-min(card_value_hand(hand)) == 4)
 
 def is_flush(hand):
     ''' flush hand function '''
@@ -25,59 +25,61 @@ def is_flush(hand):
 
 def kind(hand, n_len):
     ''' To determine the kind of the function'''
-    for ranks in crd_v(hand):
-        if crd_v(hand).count(ranks) == n_len:
+    for ranks in card_value_hand(hand):
+        if card_value_hand(hand).count(ranks) == n_len:
             return ranks
     return None
 
 # def is_four_of_a_kind(hand):
 #     ''' Four of a kind hand function '''
-#     return list(collections.Counter(crd_v(hand)).values()) == [4, 1]
+#     return list(collections.Counter(card_value_hand(hand)).values()) == [4, 1]
 
 # def is_fullhouse(hand):
 #     ''' Full House hand function '''
-#     return sorted(list(collections.Counter(crd_v(hand)).values())) == [2, 3]
+#     return sorted(list(collections.Counter(card_value_hand(hand)).values())) == [2, 3]
 
 # def is_three_of_kind(hand):
 #     ''' Three of kind hand function '''
-#     bool_val = sorted(list(collections.Counter(crd_v(hand)).values())) == [1, 1, 3]
+#     bool_val = sorted(list(collections.Counter(card_value_hand(hand)).values())) == [1, 1, 3]
 #     return bool_val
 
 # def is_twopair(hand):
 #     ''' Two pair hand function '''
-#     bool_val = sorted(list(collections.Counter(crd_v(hand)).values())) == [1, 2, 2]
+#     bool_val = sorted(list(collections.Counter(card_value_hand(hand)).values())) == [1, 2, 2]
 #     return bool_val
 
 # def is_onepair(hand):
 #     ''' One pair hand function '''
-#     bool_val = sorted(list(collections.Counter(crd_v(hand)).values())) == [1, 1, 1, 2]
+#     bool_val = sorted(list(collections.Counter(card_value_hand(hand)).values())) == [1, 1, 1, 2]
 #     return bool_val
 
 def hand_rank(hand):
     ''' ranks the card '''
-    hand_ranks = crd_v(hand)
+    hand_ranks = card_value_hand(hand)
+    print(hand_ranks)
     rank = (0, hand_ranks)
-    if is_flush(hand) and is_straight(hand):
+    if is_flush(hand) and is_straight(hand):       # straight flush
         rank = (8,)
-    # four of a kind
-    elif kind(hand, 4):
+
+    elif kind(hand, 4):                            # four of a kind
         rank = (7,)
-    # full house
-    elif kind(hand, 3) and kind(hand, 2):
+    
+    elif kind(hand, 3) and kind(hand, 2):          # full house
         rank = (6, )
-    elif is_flush(hand):
+
+    elif is_flush(hand):                           # flush
         rank = (5, hand_ranks)
-    elif is_straight(hand):
+
+    elif is_straight(hand):                        # straight
         rank = (4, hand_ranks)
-    # three of a kind
-    elif kind(hand, 3):
+
+    elif kind(hand, 3):                            # three of a kind
         return (3, kind(hand, 2), hand_ranks)
-    # Two pair
-    elif kind(hand, 2) and kind(sorted(hand, reverse=True), 2) and kind(\
-    hand, 2) != kind(sorted(hand, reverse=True), 2):
-        return (2, kind(hand, 2), kind(sorted(hand, reverse=True), 2))
-    # one pair
-    elif kind(hand, 2):
+    
+    elif kind(hand, 2) and kind(sorted(hand, reverse=True), ) and kind(hand, 2) != kind(sorted(hand, reverse=True), 2):     # Two pair
+        return (2, kind(hand, 2), kind(sorted(hand, ), 2))
+
+    elif kind(hand, 2):                            # one pair
         return (1, kind(hand, 2))
     return rank
 
