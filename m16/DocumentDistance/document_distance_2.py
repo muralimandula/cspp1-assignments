4c638c776dd44e9b71510a3c2db1036cbf933d01
muralimandula <murali.mandula@msitprogram.net>
2018-08-17 18:35:36 -0700

    " -> 3 of 3 passed. pylint: 8.11/10 "

--- m16/DocumentDistance/document_distance_2.py
+++ m16/DocumentDistance/document_distance_2.py
@@ -9,7 +9,7 @@ def clean(input):
         Make a words list and clean up the words, eliminating all special characters and numbers
     '''
     regex = re.compile('[^a-z]')                          # cap-'^' mean only, i.e., only a to z
-    print([regex.sub('', word.strip()) for word in input.lower().split(' ')])
+    # print([regex.sub('', word.strip()) for word in input.lower().split(' ')])
     return [regex.sub('', word.strip()) for word in input.lower().split(' ')]
 
 def word_frequency(list_of_words, index, dictionary = {}):
