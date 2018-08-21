d0a26138028c813b1f1bed6805e00ceef34a61e1
muralimandula <murali.mandula@msitprogram.net>
2018-08-20 22:44:35 -0700

    " -> 0 of 1 passed. pylint: 9.39/10 "

--- m18/p1/build_search_index.py
+++ m18/p1/build_search_index.py
@@ -66,12 +66,13 @@ def print_search_index(search_index):
     '''
         print the search search_index
     '''
-    keys = list(set(sorted(search_index.keys())))
+    keys = sorted(search_index.keys())
     # print(search_index)
     for key in keys:
         for i in range(6):
-            if search_index[key][i][1] == 0:
-                print(key, " - ", keys[key])
+            if keys[key][i][1] == 0:
+                del(keys[key][i])
+        print(key, " - ", keys[key])
     # for key in search_index:
     #     while (0,0) in search_index[key]:
     #         search_index
