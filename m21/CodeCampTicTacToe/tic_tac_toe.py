9ffb38bc01bdb21fdd5ce95e090f848a00f35b52
muralimandula <murali.mandula@msitprogram.net>
2018-08-24 15:56:13 -0700

    " -> 14 of 14 passed. pylint: -0.44/10 "

--- m21/CodeCampTicTacToe/tic_tac_toe.py
+++ m21/CodeCampTicTacToe/tic_tac_toe.py
@@ -31,7 +31,7 @@ def horizontal_check(game_grid):
 def vertical_check(game_grid):
 	'''checks vertical'''
 	win_count = 0
-	win_flag = game_grid[0][i]
+	win_flag = game_grid[0][0]
 	grid_length = len(game_grid)
 	for i in range(grid_length):
 		if win_count == 3:
