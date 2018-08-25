24a683d48f316e6b032727ebff11af153d453c97
muralimandula <murali.mandula@msitprogram.net>
2018-08-25 11:04:12 -0700

    " -> 0 of 4 passed. pylint: 5.38/10 "

--- m22/assignment3/tokenize.py
+++ m22/assignment3/tokenize.py
@@ -7,22 +7,19 @@ def tokenize(input_list):
     """tokenize or word frequency"""
     input_dict = {}
     for each_word in set(input_list):
-    	input_dict[each_word] = input_list.count(each_word)
+        input_dict[each_word] = input_list.count(each_word)
 
     return input_dict
 
             
 def main():
-	"""Main FUnction"""
-	no_of_lines = int(input())
-	input_list = []
-	for _ in ranage(no_of_lines):
-		input_list = input().split()   # list of lists
+    """Main FUnction"""
+    no_of_lines = int(input())
+    input_list = []
+    for _ in ranage(no_of_lines):
+        input_list = input().split()   # list of lists
 
-	print(tokenize(input_list))
-
-
-    pass
+    print(tokenize(input_list))
 
 if __name__ == '__main__':
     main()
