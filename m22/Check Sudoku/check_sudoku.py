bb3a028b0463f0327c9bca44c937b44c5d379dbb
muralimandula <murali.mandula@msitprogram.net>
2018-08-25 12:12:15 -0700

    " -> 0 of 26 passed. pylint: 2.63/10 "

--- m22/Check Sudoku/check_sudoku.py
+++ m22/Check Sudoku/check_sudoku.py
@@ -13,7 +13,45 @@ def check_sudoku(sudoku):
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
-    pass
+    for i in range(9):
+        l = sudoku[i]
+        if "".join(l.sort()) != "123456789":
+            return False
+    for i in range(9):
+        l = []
+        for j in range(9):
+            l.append(sudoku[j][i])
+        if "".join(sorted(l)) != "123456789":
+            return False
+
+    # for first Horizontal column
+    if "".join(sorted(sudoku[0][0]+sudoku[0][1]+sudoku[0][2]+ sudoku[1][0]+ sudoku[1][1]+sudoku[1][2]+sudoku[2][0]+sudoku[2][1]+sudoku[2][2]))!= "123456789":
+        return False
+    if "".join(sorted(sudoku[0][3]+sudoku[0][4]+sudoku[0][5]+ sudoku[1][3]+ sudoku[1][4]+sudoku[1][5]+sudoku[2][3]+sudoku[2][4]+sudoku[2][5]))!= "123456789":
+        return False  
+    if "".join(sorted(sudoku[0][6]+sudoku[0][7]+sudoku[0][8]+ sudoku[1][6]+ sudoku[1][7]+sudoku[1][8]+sudoku[2][6]+sudoku[2][7]+sudoku[2][8]))!= "123456789":
+        return False 
+
+
+    # below for second horizontal column
+    if "".join(sorted(sudoku[3][0]+sudoku[3][1]+sudoku[3][2]+ sudoku[4][0]+ sudoku[4][1]+sudoku[4][2]+sudoku[5][0]+sudoku[5][1]+sudoku[5][2]))!= "123456789":
+        return False
+    if "".join(sorted(sudoku[3][3]+sudoku[3][4]+sudoku[3][5]+ sudoku[4][3]+ sudoku[4][4]+sudoku[4][5]+sudoku[5][3]+sudoku[5][4]+sudoku[5][5]))!= "123456789":
+        return False
+    if "".join(sorted(sudoku[3][6]+sudoku[3][7]+sudoku[3][8]+ sudoku[4][6]+ sudoku[4][7]+sudoku[4][8]+sudoku[5][6]+sudoku[5][7]+sudoku[5][8]))!= "123456789":
+        return False 
+
+    # below for second horizontal column
+    if "".join(sorted(sudoku[3][0]+sudoku[3][1]+sudoku[3][2]+ sudoku[4][0]+ sudoku[4][1]+sudoku[4][2]+sudoku[5][0]+sudoku[5][1]+sudoku[5][2]))!= "123456789":
+        return False
+    if "".join(sorted(sudoku[3][3]+sudoku[3][4]+sudoku[3][5]+ sudoku[4][3]+ sudoku[4][4]+sudoku[4][5]+sudoku[5][3]+sudoku[5][4]+sudoku[5][5]))!= "123456789":
+        return False
+    if "".join(sorted(sudoku[3][6]+sudoku[3][7]+sudoku[3][8]+ sudoku[4][6]+ sudoku[4][7]+sudoku[4][8]+sudoku[5][6]+sudoku[5][7]+sudoku[5][8]))!= "123456789":
+        return False 
+
+
+    return True
+
 
 def main():
     '''
