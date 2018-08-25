f6cbed0dea6df65e0946dc2c64e35de7b14c800f
muralimandula <murali.mandula@msitprogram.net>
2018-08-25 12:39:31 -0700

    " -> 26 of 26 passed. pylint: 6.56/10 "

--- m22/Check Sudoku/check_sudoku.py
+++ m22/Check Sudoku/check_sudoku.py
@@ -31,7 +31,7 @@ def check_sudoku(sudoku):
     if "".join(sorted(sudoku[0][0]+sudoku[0][1]+sudoku[0][2]+ sudoku[1][0]+ sudoku[1][1]+sudoku[1][2]+sudoku[2][0]+sudoku[2][1]+sudoku[2][2])) != "123456789":
         return False
     if "".join(sorted(sudoku[0][3]+sudoku[0][4]+sudoku[0][5]+ sudoku[1][3]+ sudoku[1][4]+sudoku[1][5]+sudoku[2][3]+sudoku[2][4]+sudoku[2][5])) != "123456789":
-        return False  
+        return False
     if "".join(sorted(sudoku[0][6]+sudoku[0][7]+sudoku[0][8]+ sudoku[1][6]+ sudoku[1][7]+sudoku[1][8]+sudoku[2][6]+sudoku[2][7]+sudoku[2][8])) != "123456789":
         return False 
 
@@ -42,15 +42,15 @@ def check_sudoku(sudoku):
     if "".join(sorted(sudoku[3][3]+sudoku[3][4]+sudoku[3][5]+ sudoku[4][3]+ sudoku[4][4]+sudoku[4][5]+sudoku[5][3]+sudoku[5][4]+sudoku[5][5])) != "123456789":
         return False
     if "".join(sorted(sudoku[3][6]+sudoku[3][7]+sudoku[3][8]+ sudoku[4][6]+ sudoku[4][7]+sudoku[4][8]+sudoku[5][6]+sudoku[5][7]+sudoku[5][8])) != "123456789":
-        return False 
+        return False
 
     # below for third horizontal 3*3 column
-    if "".join(sorted(sudoku[3][0]+sudoku[3][1]+sudoku[3][2]+ sudoku[4][0]+ sudoku[4][1]+sudoku[4][2]+sudoku[5][0]+sudoku[5][1]+sudoku[5][2])) != "123456789":
-        return False
-    if "".join(sorted(sudoku[3][3]+sudoku[3][4]+sudoku[3][5]+ sudoku[4][3]+ sudoku[4][4]+sudoku[4][5]+sudoku[5][3]+sudoku[5][4]+sudoku[5][5])) != "123456789":
-        return False
-    if "".join(sorted(sudoku[3][6]+sudoku[3][7]+sudoku[3][8]+ sudoku[4][6]+ sudoku[4][7]+sudoku[4][8]+sudoku[5][6]+sudoku[5][7]+sudoku[5][8])) != "123456789":
-        return False 
+    # if "".join(sorted(sudoku[3][0]+sudoku[3][1]+sudoku[3][2]+ sudoku[4][0]+ sudoku[4][1]+sudoku[4][2]+sudoku[5][0]+sudoku[5][1]+sudoku[5][2])) != "123456789":
+    #     return False
+    # if "".join(sorted(sudoku[3][3]+sudoku[3][4]+sudoku[3][5]+ sudoku[4][3]+ sudoku[4][4]+sudoku[4][5]+sudoku[5][3]+sudoku[5][4]+sudoku[5][5])) != "123456789":
+    #     return False
+    # if "".join(sorted(sudoku[3][6]+sudoku[3][7]+sudoku[3][8]+ sudoku[4][6]+ sudoku[4][7]+sudoku[4][8]+sudoku[5][6]+sudoku[5][7]+sudoku[5][8])) != "123456789":
+    #     return False
 
 
     return True
