a2aed761ce816b7513a9ac31c516bce09aa55132
muralimandula <murali.mandula@msitprogram.net>
2018-08-25 12:22:02 -0700

    " -> 26 of 26 passed. pylint: 3.16/10 "

--- m22/Check Sudoku/check_sudoku.py
+++ m22/Check Sudoku/check_sudoku.py
@@ -13,18 +13,21 @@ def check_sudoku(sudoku):
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
+    # horizontal
     for i in range(9):
-        l = sudoku[i]
-        if "".join(l.sort()) != "123456789":
-            return False
-    for i in range(9):
-        l = []
-        for j in range(9):
-            l.append(sudoku[j][i])
-        if "".join(sorted(l)) != "123456789":
+        horizontal_rows = sudoku[i]
+        if "".join(sorted(horizontal_rows)) != "123456789":
             return False
 
-    # for first Horizontal column
+    # vertical columns
+    for i in range(9):
+        vertical_coliumns = []
+        for j in range(9):
+            vertical_coliumns.append(sudoku[j][i])
+        if "".join(sorted(vertical_coliumns)) != "123456789":
+            return False
+
+    # for first Horizontal 3*3 grid
     if "".join(sorted(sudoku[0][0]+sudoku[0][1]+sudoku[0][2]+ sudoku[1][0]+ sudoku[1][1]+sudoku[1][2]+sudoku[2][0]+sudoku[2][1]+sudoku[2][2]))!= "123456789":
         return False
     if "".join(sorted(sudoku[0][3]+sudoku[0][4]+sudoku[0][5]+ sudoku[1][3]+ sudoku[1][4]+sudoku[1][5]+sudoku[2][3]+sudoku[2][4]+sudoku[2][5]))!= "123456789":
@@ -33,7 +36,7 @@ def check_sudoku(sudoku):
         return False 
 
 
-    # below for second horizontal column
+    # below for second horizontal 3*3 column
     if "".join(sorted(sudoku[3][0]+sudoku[3][1]+sudoku[3][2]+ sudoku[4][0]+ sudoku[4][1]+sudoku[4][2]+sudoku[5][0]+sudoku[5][1]+sudoku[5][2]))!= "123456789":
         return False
     if "".join(sorted(sudoku[3][3]+sudoku[3][4]+sudoku[3][5]+ sudoku[4][3]+ sudoku[4][4]+sudoku[4][5]+sudoku[5][3]+sudoku[5][4]+sudoku[5][5]))!= "123456789":
@@ -41,7 +44,7 @@ def check_sudoku(sudoku):
     if "".join(sorted(sudoku[3][6]+sudoku[3][7]+sudoku[3][8]+ sudoku[4][6]+ sudoku[4][7]+sudoku[4][8]+sudoku[5][6]+sudoku[5][7]+sudoku[5][8]))!= "123456789":
         return False 
 
-    # below for second horizontal column
+    # below for third horizontal 3*3 column
     if "".join(sorted(sudoku[3][0]+sudoku[3][1]+sudoku[3][2]+ sudoku[4][0]+ sudoku[4][1]+sudoku[4][2]+sudoku[5][0]+sudoku[5][1]+sudoku[5][2]))!= "123456789":
         return False
     if "".join(sorted(sudoku[3][3]+sudoku[3][4]+sudoku[3][5]+ sudoku[4][3]+ sudoku[4][4]+sudoku[4][5]+sudoku[5][3]+sudoku[5][4]+sudoku[5][5]))!= "123456789":
