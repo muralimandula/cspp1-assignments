590200fd4b6b08a8671cbe0fd9d2bbcc1758ed3d
muralimandula <murali.mandula@msitprogram.net>
2018-08-25 11:03:27 -0700

    " -> 0 of 4 passed. pylint: 0/0 "

--- m22/assignment3/tokenize.py
+++ m22/assignment3/tokenize.py
@@ -3,10 +3,25 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 
-def tokenize(string):
-    pass
+def tokenize(input_list):
+    """tokenize or word frequency"""
+    input_dict = {}
+    for each_word in set(input_list):
+    	input_dict[each_word] = input_list.count(each_word)
+
+    return input_dict
+
             
 def main():
+	"""Main FUnction"""
+	no_of_lines = int(input())
+	input_list = []
+	for _ in ranage(no_of_lines):
+		input_list = input().split()   # list of lists
+
+	print(tokenize(input_list))
+
+
     pass
 
 if __name__ == '__main__':
