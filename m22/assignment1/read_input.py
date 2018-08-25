33f159a137ebd616d84d50dfc3a77f6625648027
muralimandula <murali.mandula@msitprogram.net>
2018-08-25 10:37:49 -0700

    " -> 0 of 2 passed. pylint: 2.50/10 "

--- m22/assignment1/read_input.py
+++ m22/assignment1/read_input.py
@@ -3,12 +3,12 @@ Write a python program to read multiple lines of text input and store the input 
 '''
 
 def main():
-	"""main function"""
+    """main function"""
     no_of_lines = int(input)
     for _ in range(no_of_lines):
-    	string=""
-    	string.append(input)
-    	print(string)
+        string=""
+        string.append(input)
+        print(string)
 
 
 if __name__ == '__main__':
