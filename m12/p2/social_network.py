4a07768a3c4ce100339d8a1c66c1aa7d5eda9af1
muralimandula <murali.mandula@msitprogram.net>
2018-08-27 09:49:45 +0530

    " -> 1 of 2 passed. pylint: 9.33/10 "

--- m12/p2/social_network.py
+++ m12/p2/social_network.py
@@ -57,7 +57,7 @@ def main():
     '''
         handling testcase input and printing command
     '''
-    network = input()
+    network = eval(input())
     # print(network)
     no_of_commands = int(input())
     for _ in range(int(no_of_commands)):
