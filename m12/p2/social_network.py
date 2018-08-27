4b451405ce1f977eb3238a3d9e1276d14483d692
muralimandula <murali.mandula@msitprogram.net>
2018-08-27 09:32:38 +0530

    " -> 0 of 2 passed. pylint: 10.00/10 "

--- m12/p2/social_network.py
+++ m12/p2/social_network.py
@@ -66,7 +66,7 @@ def main():
         if command_split[0] == "follow":
             network1 = follow(network, command_split[1], command_split[2])
         elif command_split[0] == "unfollow":
-            network = unfollow(network, command_split[1], command_split[2])
+            network1 = unfollow(network, command_split[1], command_split[2])
         elif command_split[0] == "delete":
             network1 = delete_person(network1, command_split[1])
 
